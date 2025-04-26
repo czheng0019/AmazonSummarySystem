a52554ca3637eecbe065272ca773ba51362dc481
TheLongPatrol <nolanwangg@gmail.com>
2025-04-26 14:51:50 -0500

    Add mapping to directly fix dataset with filtered product names

--- clustering.py
+++ clustering.py
@@ -8,27 +8,33 @@ class ProductNames():
         word_to_freq = {}
         for product_name in unfiltered_product_names:
             for word in product_name.split():
-                if len(word) > 1:
+                if len(word) > 2:
                     word_to_freq[word] = word_to_freq.get(word, 0)+1
         max_freq = max(word_to_freq.values())
         freq_to_words = [[] for _ in range(max_freq+1)]
         for word in word_to_freq.keys():
             freq_to_words[word_to_freq[word]].append(word)
-        top_500 = []
+        top_300 = []
         ind = max_freq
-        while len(top_500) < 500 and ind > -1:
-            top_500 = top_500 + freq_to_words[ind]
+        while len(top_300) < 300 and ind > -1:
+            top_300 = top_300 + freq_to_words[ind]
             ind-=1
         print("Stopped before this freq:",ind)
-        print("vocab size", len(top_500))
-        self.most_common_product_words = set(top_500)
+        print("vocab size", len(top_300))
+        self.most_common_product_words = set(top_300)
         self.filtered_product_names = []
+        unfiltered_to_filtered_mapping = {}
         for product_name in unfiltered_product_names:
             filtered_words = []
             for word in product_name.split():
                 if word in self.most_common_product_words:
                     filtered_words.append(word)
-            self.filtered_product_names.append(' '.join(filtered_words))
+            filtered_product_name = ' '.join(filtered_words)
+            if len(filtered_words) > 0:
+                unfiltered_to_filtered_mapping[product_name] = filtered_product_name
+                self.filtered_product_names.append(filtered_product_name)
+        data["product name"] = data["product name"].map(unfiltered_to_filtered_mapping)
+        self.filtered_dataset = data
 if __name__ == "__main__":
     start_time = time.time()
     final_product_names = ProductNames("./data/processed_data.csv")
@@ -36,6 +42,8 @@ if __name__ == "__main__":
     print("time taken", end_time - start_time)
     print(final_product_names.filtered_product_names[:10])
     print(final_product_names.filtered_product_names[-10:])
+    print(len(final_product_names.filtered_product_names), len(set(final_product_names.filtered_product_names)))
+    print(final_product_names.filtered_dataset.head())
     # product_names = pd.unique(data['product_name'])
     # print(product_names.shape)
     # for product_name in product_names:
