e8bcc7b89a4ffc7c72f2a149e26528e2d77bcc42
TheLongPatrol <nolanwangg@gmail.com>
2025-04-26 15:58:41 -0500

    Add clustering code

--- clustering.py
+++ clustering.py
@@ -41,18 +41,48 @@ class ProductNames():
             self.filtered_product_names.append(filtered_product_name)
         data["product name"] = data["product name"].map(unfiltered_to_filtered_mapping)
         self.filtered_dataset = data
+    def create_clusters(self):
+        unique_product_names = pd.unique(self.filtered_dataset['product name'])
+        data = self.filtered_dataset
+        product_name_clusters = {}
+        
+        for product_name in unique_product_names:
+            word_to_scores = {}
+            word_to_num_reviews = {}
+            reviews_for_product = data[data["product name"] == product_name]
+            for index, row in reviews_for_product.iterrows():
+                rating = row[0]
+                text = row[1]
+                if not isinstance(text, str):
+                    continue
+                seen_words = set()
+                for word in text.split():
+                    if word not in seen_words:
+                        word_to_scores[word] = word_to_scores.get(word, 0)+rating
+                        seen_words.add(word)
+                        word_to_num_reviews[word] = word_to_num_reviews.get(word,0)+1
+            average_score_one = []
+            average_score_five = []
+            for word in word_to_scores:
+                avg_score = word_to_scores[word]/len(word_to_num_reviews[word])
+                if avg_score < 1.5:
+                    average_score_one.append(word)
+                elif avg_score > 4.5:
+                    average_score_five.append(word)
+            product_name_clusters[product_name] = [average_score_one, average_score_five]
+        self.product_name_clusters = product_name_clusters
+        
+
 if __name__ == "__main__":
     start_time = time.time()
     final_product_names = ProductNames("./data/processed_data_nouns.csv")
     end_time = time.time()
-    print("time taken", end_time - start_time)
+    print("time taken to build vocab", end_time - start_time)
     print(final_product_names.filtered_product_names[:10])
     print(final_product_names.filtered_product_names[-10:])
     print(len(final_product_names.filtered_product_names), len(set(final_product_names.filtered_product_names)))
     print(final_product_names.filtered_dataset.head())
-    # product_names = pd.unique(data['product_name'])
-    # print(product_names.shape)
-    # for product_name in product_names:
-    #     relevant_reviews = data[data['product_name']==product_name]
-    # print("done")
-    # print(data.groupby(["product name"]).head())
+    start_time = time.time()
+    final_product_names.create_clusters()
+    end_time = time.time()
+    print("time taken to cluster", end_time - start_time)
