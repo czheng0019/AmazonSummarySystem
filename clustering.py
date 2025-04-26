d3d15d301bf21c359acc5358b1f53b1cd92cc92b
TheLongPatrol <nolanwangg@gmail.com>
2025-04-26 14:22:40 -0500

    Keep 500 most common words in product names

---
+++ clustering.py
@@ -0,0 +1,44 @@
+import pandas as pd 
+import time
+class ProductNames():
+    def __init__(self, datafile):
+        data = pd.read_csv(datafile, header=0)
+        unfiltered_product_names = pd.unique(data['product name']).astype(str)
+        #print(unfiltered_product_names)
+        word_to_freq = {}
+        for product_name in unfiltered_product_names:
+            for word in product_name.split():
+                if len(word) > 1:
+                    word_to_freq[word] = word_to_freq.get(word, 0)+1
+        max_freq = max(word_to_freq.values())
+        freq_to_words = [[] for _ in range(max_freq+1)]
+        for word in word_to_freq.keys():
+            freq_to_words[word_to_freq[word]].append(word)
+        top_500 = []
+        ind = max_freq
+        while len(top_500) < 500 and ind > -1:
+            top_500 = top_500 + freq_to_words[ind]
+            ind-=1
+        print("Stopped before this freq:",ind)
+        print("vocab size", len(top_500))
+        self.most_common_product_words = set(top_500)
+        self.filtered_product_names = []
+        for product_name in unfiltered_product_names:
+            filtered_words = []
+            for word in product_name.split():
+                if word in self.most_common_product_words:
+                    filtered_words.append(word)
+            self.filtered_product_names.append(' '.join(filtered_words))
+if __name__ == "__main__":
+    start_time = time.time()
+    final_product_names = ProductNames("./data/processed_data.csv")
+    end_time = time.time()
+    print("time taken", end_time - start_time)
+    print(final_product_names.filtered_product_names[:10])
+    print(final_product_names.filtered_product_names[-10:])
+    # product_names = pd.unique(data['product_name'])
+    # print(product_names.shape)
+    # for product_name in product_names:
+    #     relevant_reviews = data[data['product_name']==product_name]
+    # print("done")
+    # print(data.groupby(["product name"]).head())
